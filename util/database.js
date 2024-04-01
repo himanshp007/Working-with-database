bb0efebdcc7d2ce4fad5944f46bd56e764e1253b
himanshp007 <himanshpatel192@gmail.com>
2024-04-01 05:39:24 +0530

    added the database to node app

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Himansh@007',
+})
+
+module.exports = pool.promise();
