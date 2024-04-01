aa6c38d93bd3ab00e21b18d199de0808cb872a45
himanshp007 <himanshpatel192@gmail.com>
2024-04-01 23:56:02 +0530

    added sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'Himansh@007', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Himansh@007',
-})
+});
 
-module.exports = pool.promise();
+module.exports = sequelize;
